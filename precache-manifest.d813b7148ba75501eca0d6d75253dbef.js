6ed81e8a0ea57daf33f87fec5e99815d45b5608b
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-11-09 21:02:30 -0600

    Updates

---
+++ precache-manifest.d813b7148ba75501eca0d6d75253dbef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gihub-pages-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9f3796011b9b451b104c",
+    "url": "/react-gihub-pages-test/static/js/runtime~main.9f379601.js"
+  },
+  {
+    "revision": "c9082ee4dbac1c3cea9e",
+    "url": "/react-gihub-pages-test/static/js/main.c9082ee4.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/react-gihub-pages-test/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "c9082ee4dbac1c3cea9e",
+    "url": "/react-gihub-pages-test/static/css/main.810ae55e.chunk.css"
+  },
+  {
+    "revision": "53e0cb0680720c6957b78ca6b9cf44ce",
+    "url": "/react-gihub-pages-test/index.html"
+  }
+];
